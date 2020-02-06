a052367a9b13efc1fc6ae35217240f53ff6fadbb
lauriane <laurianeegouraud@gmail.com>
2020-02-06 18:58:20 +0100

    MAJ navbarre / footer

--- SiteParfumerie_Developpement/BarreNav.php
+++ SiteParfumerie_Developpement/BarreNav.php
@@ -87,5 +87,65 @@
         </div>
       </div>
   </nav>
+
+
+
+
+  <div>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+  </div>
+
+
+
+  <!-- Footer : Lien non fonctionnels-->
+<footer class="text-light">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="justify-content-start espaceGrand">
+                <h5>Plan du site</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-50">
+                <ul class="list-unstyled">
+                    <li><a href="">Liste des clients</a></li>
+                    <li><a href="">Liste Client</a></li>
+                    <li><a href="">Liste des commandes d'un client</a></li>
+                    <li><a href="">Information produit</a></li>
+                    <li><a href="">Information client</a></li>
+                    <li><a href="">Ajouter ou modifier un produit</a></li>
+                    <li><a href="">Ajouter ou modifier un client</a></li>
+                    <li><a href="">Recapitulatif de commande</a></li>
+                </ul>
+            </div>
+
+            <div class="justify-content-end">
+                <h5>Contact</h5>
+                <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-50">
+                <ul class="list-unstyled">
+                    <li><i class="fa fa-user mr-2"></i> AZHARI Abderrhaman</li>
+                    <li><i class="fa fa-user mr-2"></i> DRIDI Ghada</li>
+                    <li><i class="fa fa-user mr-2"></i> GOURAUD Lauriane</li>
+                    <li><i class="fa fa-user mr-2"></i> VALLÉE Lilian</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
+
 </body>
 </html>
