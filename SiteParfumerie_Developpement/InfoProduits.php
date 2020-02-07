e8b31e0f46cf935fc3e639a0127cb2a16b91e0cb
lauriane <laurianeegouraud@gmail.com>
2020-02-07 20:10:53 +0100

    Modif nav / footer / SQL de InfoProduits.php

--- SiteParfumerie_Developpement/InfoProduits.php
+++ SiteParfumerie_Developpement/InfoProduits.php
@@ -1,6 +1,6 @@
 <?php
 $mysqli=new mysqli('localhost','root','','bd_parfumerie');
-$query='select * from produit';
+$query='SELECT * FROM `produit` WHERE `id_produit`='.$_GET['Id'].';';
 $result=$mysqli->query($query);
 ?>
 
@@ -22,65 +22,93 @@ $result=$mysqli->query($query);
 
 <body>
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="index.html">Simple Ecommerce</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-                aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
-                <ul class="navbar-nav m-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="index.html">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="category.html">Categories</a>
-                    </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="product.html">Product <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="cart.html">Cart</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact.html">Contact</a>
-                    </li>
-                </ul>
-
-                <form class="form-inline my-2 my-lg-0">
-                    <div class="input-group input-group-sm">
-                        <input type="text" class="form-control" placeholder="Search...">
-                        <div class="input-group-append">
-                            <button type="button" class="btn btn-secondary btn-number">
-                                <i class="fa fa-search"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <a class="btn btn-success btn-sm ml-3" href="cart.html">
-                        <i class="fa fa-shopping-cart"></i> Cart
-                        <span class="badge badge-light">3</span>
-                    </a>
-                </form>
-
-            </div>
-        </div>
-    </nav>
-
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark margBot">
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="/">Home</a></li>
-                        <li class="breadcrumb-item"><a href="category.html">Category</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Product</li>
-                    </ol>
-                </nav>
+      <div class="collapse navbar-collapse justify-content-start">
+        <?php
+          $requete = "SELECT `nom`, `prenom` FROM `client` INNER JOIN `client_actif` ON `id_client`=`id_client_actif`";
+          $resultat = $mysqli->query($requete);
+          $val=$resultat->fetch_assoc();
+          if($val == null)
+          {
+            echo'
+              <div class="espace front_bar_nav navbar-collapse align-items-center">
+                <img class="icon espace" src="IconesSite\user.png">
+                  <a class="btn btn-secondary btn-number espace" href="ListeClient.php">
+                    <i class="fa fa-sign-in" aria-hidden="true">
+                      Connecter client
+                    </i>
+                  </a>
+              </div>
+            ';
+          }
+          else
+          {
+            echo'
+              <div class="espace">
+                <a href="PageClient.php" class="navbar-brand bouton">
+                  <img class="icon" src="IconesSite\user.png">
+                    '.$val['nom'].' '.$val['prenom'].'
+                </a>
+                <a class="btn btn-secondary btn-number espace" href="ExeDecoClient.php">
+                  <i class="fa fa-sign-out" aria-hidden="true">
+                    Deconnecter client
+                  </i>
+                </a>
+              </div>
+            ';
+          }
+        ?>
+      </div>
+        <form class="collapse navbar-collapse justify-content-center widthSearch">
+            <div class="input-group input-group-sm">
+                <input type="text" class="form-control" placeholder="Search a product">
+                <div class="input-group-append">
+                    <button onclick="window.location.href = 'RechercheProduit.php';" type="button" class="btn btn-secondary btn-number" >
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
             </div>
+        </form>
+
+        <div class="collapse navbar-collapse justify-content-end">
+          <div class="form-inline my-2 my-lg-0 espace" id="navbarsExampleDefault">
+              <a class="btn btn-success btn-sm ml-3" href="cart.html">
+                      <i class="fa fa-shopping-cart"></i> Cart
+                      <span class="badge badge-light">
+                        <?php
+                          $requete = "SELECT COUNT(id_article) AS nbArticles FROM `articles_commandes` NATURAL JOIN `commande` JOIN `client_actif` ON `id_client`=`id_client_actif` WHERE `etat_commande` = 'Current being processed'";
+                          $resultat = $mysqli->query($requete);
+                          $val=$resultat->fetch_assoc();
+                          if($val == null)
+                          {
+                            echo'
+                              0
+                            ';
+                          }
+                          else
+                          {
+                            echo''.
+                              $val['nbArticles'].'
+                            ';
+                          }
+                        ?>
+                      </span>
+              </a>
+          </div>
+          <a class="btn btn-secondary btn-number" href="ExeDecoConcierge.php"> <!--Voir si on laisse decoUser ou si on met une autre page -->
+            <i class="fa fa-sign-out" aria-hidden="true">
+              Deconnection
+            </i>
+
+          </a>
         </div>
-    </div>
+      </div>
+  </nav>
+
+
+
     <div class="container">
         <div class="row">
             <!-- Image -->
@@ -94,12 +122,13 @@ $result=$mysqli->query($query);
 
                         </a>
                         </br>
-                        <?php if($result!=null){
-        while($product=$result->fetch_assoc()){
-echo'
+                        <?php
+                        if($result!=null){
+                          while($product=$result->fetch_assoc()){
+                            echo'
 
                         <p class="text-center">'.$product['nom_article'].'</p>
-                                 
+
                     </div>
                 </div>
             </div>
@@ -131,11 +160,11 @@ echo'
                                     <th>Ingredients</th>
                                     <th class="form-group">'.$product['ingredients'].'</th>
                                   </tr>
-                                  
+
 
                             </table>
                            ' ; }}?>
-                          
+
 
                         </form>
 
@@ -149,60 +178,39 @@ echo'
 
     <!-- Footer -->
     <footer class="text-light">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3 col-lg-4 col-xl-3">
-                    <h5>About</h5>
-                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
-                    <p class="mb-0">
-                        Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant
-                        impression.
-                    </p>
-                </div>
-
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto">
-                    <h5>Informations</h5>
-                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
-                    <ul class="list-unstyled">
-                        <li><a href="">Link 1</a></li>
-                        <li><a href="">Link 2</a></li>
-                        <li><a href="">Link 3</a></li>
-                        <li><a href="">Link 4</a></li>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="justify-content-start espaceGrand">
+            <h5>Plan du site</h5>
+            <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-50">
+            <ul class="list-unstyled">
+              <li class="interligne-2"><a href="">Liste des clients</a></li>
+              <li class="interligne-2"><a href="">Liste Client</a></li>
+              <li class="interligne-2"><a href="">Liste des commandes d'un client</a></li>
+              <li class="interligne-2"><a href="InfoProduits.php">Information produit</a></li>
+              <li class="interligne-2"><a href="">Information client</a></li>
+              <li class="interligne-2"><a href="">Ajouter ou modifier un produit</a></li>
+              <li class="interligne-2"><a href="AddModifClient.php">Ajouter ou modifier un client</a></li>
+              <li class="interligne-2"><a href="">Recapitulatif de commande</a></li>
                     </ul>
-                </div>
-
-                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto">
-                    <h5>Others links</h5>
-                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
-                    <ul class="list-unstyled">
-                        <li><a href="">Link 1</a></li>
-                        <li><a href="">Link 2</a></li>
-                        <li><a href="">Link 3</a></li>
-                        <li><a href="">Link 4</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 col-lg-3 col-xl-3">
-                    <h5>Contact</h5>
-                    <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-25">
-                    <ul class="list-unstyled">
-                        <li><i class="fa fa-home mr-2"></i> My company</li>
-                        <li><i class="fa fa-envelope mr-2"></i> email@example.com</li>
-                        <li><i class="fa fa-phone mr-2"></i> + 33 12 14 15 16</li>
-                        <li><i class="fa fa-print mr-2"></i> + 33 12 14 15 16</li>
-                    </ul>
-                </div>
-                <div class="col-12 copyright mt-3">
-                    <p class="float-left">
-                        <a href="#">Back to top</a>
-                    </p>
-                    <p class="text-right text-muted">created with <i class="fa fa-heart"></i> by <a
-                            href="https://t-php.fr/43-theme-ecommerce-bootstrap-4.html"><i>t-php</i></a> | <span>v.
-                            1.0</span></p>
-                </div>
-            </div>
         </div>
+
+          <div class="justify-content-end">
+          <h5>Contact</h5>
+          <hr class="bg-white mb-2 mt-0 d-inline-block mx-auto w-50">
+          <ul class="list-unstyled">
+            <li class="interligne-2"><i class="fa fa-user mr-2"></i> AZHARI Abderrhaman</li>
+            <li class="interligne-2"><i class="fa fa-user mr-2"></i> DRIDI Ghada</li>
+            <li class="interligne-2"><i class="fa fa-user mr-2"></i> GOURAUD Lauriane</li>
+            <li class="interligne-2"><i class="fa fa-user mr-2"></i> VALLÉE Lilian</li>
+          </ul>
+        </div>
+        </div>
+      </div>
     </footer>
+
+
+
 
 
     <!-- Modal image -->
